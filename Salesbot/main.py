4cd9b2b9ed42c9441cec42e0a6134d7a985a0af1
wtao <wtao>
2023-09-04 15:54:41 -0700

    exit loop

--- Salesbot/main.py
+++ Salesbot/main.py
@@ -10,7 +10,7 @@ import os
 load_dotenv()
 
 
-def buildChain(llm):
+def buildAgentChain(llm):
     template = PromptTemplate(
         template=prompts.SALES_AGENT_TOOLS_PROMPT,
         input_variables=[
@@ -26,27 +26,52 @@ def buildChain(llm):
     )
     return LLMChain(prompt=template, llm=llm, verbose=False)
 
+def buildAssitantChain(llm):
+    template = PromptTemplate(
+        template=prompts.STAGE_ANALYZER_INCEPTION_PROMPT,
+        input_variables=[
+            "conversation_history",
+            "conversation_stage_id",
+            "conversation_stages",
+        ],
+    )
+    chain = LLMChain(prompt=template, llm=llm)
+    stages = list(map(lambda item: f'{item[0]}: {item[1]}', prompts.CONVERSATION_STAGES.items()))
+    return (chain, stages)
+
 
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')
 
     llm = ChatOpenAI(model="gpt-3.5-turbo")
-    chain = buildChain(llm)
-    stage = '1'
+    agentChain = buildAgentChain(llm)
+    assistantChain, stages = buildAssitantChain(llm)
+    currentSalesStage = '1'
     conversationHisotry = []
 
     while True:
         prompt = example.ExampleSales.copy()
         prompt['conversation_history'] = "\n".join(conversationHisotry)
         
-        aiMessage = chain.__call__(inputs=prompt)
-        output = f'{example.ExampleSales["salesperson_name"]}: {aiMessage["text"]}'
-        conversationHisotry.append(output)
+        aiMessage = agentChain.__call__(inputs=prompt)
+        conversationHisotry.append(aiMessage["text"])
         print('\x1b[35m')
-        print(output.replace("<END_OF_TURN>", ""))
+        print(aiMessage["text"].replace("<END_OF_TURN>", ""))
         print('\x1b[0m')
+
+        if '<END_OF_CALL>' in aiMessage["text"]:
+            break
 
         userMessage = input("Your response: ")
         conversationHisotry.append(f'User: {userMessage} <END_OF_TURN>')
+
+        conversationStage = assistantChain.run(
+            conversation_history="\n".join(conversationHisotry).rstrip("\n"),
+            conversation_stage_id=currentSalesStage,
+            conversation_stages="\n".join(stages),
+        )
+        print('\x1b[34m')
+        print(conversationStage)
+        print('\x1b[0m')
 
 
--- Salesbot/prompts.py
+++ Salesbot/prompts.py
@@ -4,7 +4,19 @@ SALES_AGENT_TOOLS_PROMPT = """
     Company values are the following. {company_values}
     You are contacting a potential prospect in order to {conversation_purpose}
     Your means of contacting the prospect is {conversation_type}. 
+
+    The company has two subscriptionm products to offer,
+
+    1. PlutoTV mopnthly subscription
+    Description: Watch in Full HD (1080p) with select titles in 4K HDR with Dolby Atmos. Stream on 1 screens at a time. Pay monthly and cancel at any time,
+    Price: $14.99
     
+    2. PlutoTV annualy subscription
+    Description: Watch in 4K UHD. Stream on 3 screens at a time. ay annually and cancel at any time',
+    Price: $99.99
+
+    end of producuts
+
     If you're asked about where you got the user's contact information, say that you got it from public records.
     Keep your responses in short length to retain the user's attention. Never produce lists, just answers.
     
@@ -69,3 +81,14 @@ STAGE_ANALYZER_INCEPTION_PROMPT = """
     The answer needs to be one number only, no words.
     Do not answer anything else nor add anything to you answer.
     """
+
+CONVERSATION_STAGES = {
+    "1": "Introduction: Start the conversation by introducing yourself and your company. Be polite and respectful while keeping the tone of the conversation professional. Your greeting should be welcoming. Always clarify in your greeting the reason why you are calling.",
+    "2": "Qualification: Qualify the prospect by confirming if they are the right person to talk to regarding your product/service. Ensure that they have the authority to make purchasing decisions.",
+    "3": "Value proposition: Briefly explain how your product/service can benefit the prospect. Focus on the unique selling points and value proposition of your product/service that sets it apart from competitors.",
+    "4": "Needs analysis: Ask open-ended questions to uncover the prospect's needs and pain points. Listen carefully to their responses and take notes.",
+    "5": "Solution presentation: Based on the prospect's needs, present your product/service as the solution that can address their pain points.",
+    "6": "Objection handling: Address any objections that the prospect may have regarding your product/service. Be prepared to provide evidence or testimonials to support your claims.",
+    "7": "Close: Ask for the sale by proposing a next step. This could be a demo, a trial or a meeting with decision-makers. Ensure to summarize what has been discussed and reiterate the benefits.",
+    "8": "End conversation: It's time to end the call as there is nothing else to be said.",
+}
